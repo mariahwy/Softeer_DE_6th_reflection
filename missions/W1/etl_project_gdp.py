f51d5a7f9b90caba00d8eaa90798d76327bf666e
mariahwy <124486478+mariahwy@users.noreply.github.com>
2025-07-11 15:01:29 +0900

    W1M3 최종

---
+++ missions/W1/etl_project_gdp.py
@@ -0,0 +1,249 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+def log_write(f, message):
+    timestamp = datetime.now().strftime("%Y-%B-%d-%H-%M-%S")
+    f.write(f"[{timestamp}]    {message}\n")
+
+
+def extract_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        raise RuntimeError(f"Falied to fetch webpage. Status Code: {response.status_code}")
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+        
+    # GDP 정보를 포함하는 table tag 찾기
+    table = soup.find("table", class_="wikitable")
+    if table is None:
+        raise RuntimeError("Table not found.")
+    
+    # 해당 table에서 실제 데이터를 담고 있는 tbody tag 찾기
+    tbody = table.find("tbody")
+    if tbody is None:
+        raise RuntimeError("tbody is not found.")
+    
+    # 테이블의 각 row 정보를 담고 있는 tr tag 찾기
+    trs = tbody.find_all("tr")
+    if not trs or len(trs) < 2:
+        raise RuntimeError("No rows found in table.")
+
+    # 테이블의 header에서 IMF에 해당하는 위치 정보만 추출
+    header = trs[0].find_all("th")
+
+    imf_idx = None
+    imf_colspan = 1
+    imf_cols = ['Country']
+
+    for idx, th in enumerate(header[1:]):
+        if "IMF" in th.get_text():
+            imf_idx = idx
+            imf_colspan = int(th.get("colspan", 1))
+            break
+    
+    if imf_idx is None or imf_idx < 0:
+        raise ValueError("IMF header is not found.")
+
+    # 테이블의 subheader에서 컬럼명을 추출
+    subheader = trs[1].find_all("th")
+    if not subheader:
+        raise RuntimeError("Subheader is not found.")
+
+    imf_cols.extend([subheader[imf_idx + i].get_text() for i in range(imf_colspan)])
+
+    # 4. tr을 반복적으로 순회하며 값 가져오기
+    rows = []
+    for tr in trs[2:]:
+        tds = tr.find_all("td")
+        if len(tds) < imf_idx + imf_colspan:
+            continue
+        
+        row = {}
+        for i in range(imf_colspan + 1):
+            colname = imf_cols[i]
+            value = tds[imf_idx + i].get_text(strip=True)
+            row[colname] = value
+
+        rows.append(row)
+
+    if not rows:
+        raise RuntimeError("No data rows were extracted.")
+        
+    df = pd.DataFrame(rows)
+    return df
+        
+def transform_data(df):
+    # df = pd.read_json("/Users/admin/Desktop/Softeer_DE_6th/missions/W1/Countries_by_GDP.json")
+
+    # world 행 제거
+    df = df[df["Country"] != "World"]
+
+    # 불필요한 문자 제거 및 결측값 처리
+    df = df.replace(r'[-—]', None, regex=True)
+    df = df.replace(r'\[.*?\]', '', regex=True)
+    df = df.replace(r',', '', regex=True)
+
+    # Forecast 열의 형 변환 및 단위 변경 (1B USD)
+    df['Forecast'] = df['Forecast'].astype(float)
+    df['Forecast'] = (df['Forecast'] / 1000).round(2)
+
+    # Year 열의 형 변환
+    df['Year'] = df['Year'].astype('Int64')
+
+    # GDP가 높은 순서대로 정렬
+    df = df.sort_values(by='Forecast', ascending=False)
+
+    # 컬럼명 수정
+    df = df.rename(columns = {'Forecast' : 'GDP_USD_billion'})
+
+    return df
+
+def load_data(df, save_path):
+    df.to_json(save_path + "Countries_by_GDP.json", orient="records", indent=4, force_ascii = False)
+    
+
+def get_region(url):
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise RuntimeError(f"Falied to fetch webpage. Status Code: {response.status_code}")
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Region 정보를 포함하는 table tag 찾기
+    table = soup.find("table", class_="wikitable")
+    if table is None:
+        raise RuntimeError("Table not found.")
+    
+    # 테이블의 각 row 정보를 담고 있는 tr tag 찾기
+    trs = table.find_all("tr")
+    if not trs or len(trs) < 2:
+        raise RuntimeError("No rows found in table.")
+    
+    header_idx = None
+    ths = trs[0].find_all("th")
+    for idx, th in enumerate(ths):
+        if 'Country' in th.get_text():
+            header_idx = idx
+            break
+
+    if header_idx == None :
+        raise RuntimeError("No header found in table.")
+
+    country_list = []
+    for tr in trs[1:]:
+        tds = tr.find_all("td")
+        if len(tds) < 2 or tds[0].get_text().strip() == "—":
+            continue
+        country = tds[header_idx].get_text(strip = True)
+        country_list.append(country)
+
+    return country_list
+
+def extract_region():
+    url_dict = {'Africa' : 'https://en.wikipedia.org/wiki/List_of_African_countries_by_GDP_(nominal)',
+                'Arab' : 'https://en.wikipedia.org/wiki/List_of_Arab_League_countries_by_GDP_(nominal)',
+                'Asia' : 'https://en.wikipedia.org/wiki/List_of_countries_in_Asia-Pacific_by_GDP_(nominal)',
+                'Latin' : 'https://en.wikipedia.org/wiki/List_of_Latin_American_and_Caribbean_countries_by_GDP_(nominal)',
+                'North America' : 'https://en.wikipedia.org/wiki/List_of_North_American_countries_by_GDP_(nominal)',
+                'Oceania' : 'https://en.wikipedia.org/wiki/List_of_Oceanian_countries_by_GDP',
+                'Europe' : 'https://en.wikipedia.org/wiki/List_of_sovereign_states_in_Europe_by_GDP_(nominal)'
+                }
+    
+    # country 열, region 열로 구성된 row 구성
+    rows = []
+    for region, url in url_dict.items():
+        country_list = get_region(url)
+
+        for country in country_list:
+            rows.append({'Country' : country, 'Region': region})
+
+    df = pd.DataFrame(rows)
+    return df
+
+def transform_region(df):
+    # 불필요한 문자 제거 및 결측값 처리
+    df = df.replace(r'[-—]', None, regex=True)
+    df = df.replace(r'\[.*?\]', '', regex=True)
+    df = df.replace(r',', '', regex=True)
+
+    return df
+
+def load_region(df, save_path):
+    df.to_json(save_path + "Countries_with_region.json", orient="records", indent=4, force_ascii = False)
+
+def filter_data(path):
+    gdp_df = pd.read_json(path + "Countries_by_GDP.json")
+    region_df = pd.read_json(path + "Countries_with_region.json")
+
+    # GDP가 100B USD 이상이 되는 국가만 구해서 화면에 출력
+    filtered_df = gdp_df[gdp_df["GDP_USD_billion"] > 100]
+    print("GDP가 100B USD 이상이 되는 국가:")
+    print(filtered_df[['Country', 'GDP_USD_billion']])
+
+    # 각 Region별로 top5 국가의 평균을 구해서 화면에 출력
+
+     # 두 데이터를 Country 기준으로 merge
+    merged_df = pd.merge(gdp_df, region_df, on="Country")
+    region_group = merged_df.groupby("Region")
+    
+    top_5_means = []
+    for region, group in region_group:
+        sorted_df = group.sort_values(by = "GDP_USD_billion", ascending = False)
+        top_5 = sorted_df.head(5)
+        mean_GDP = top_5["GDP_USD_billion"].mean()
+        top_5_means.append((region, mean_GDP))
+
+    top_5_means_df = pd.DataFrame(top_5_means, columns=['Region', 'GDP_means'])
+    top_5_means_df['GDP_means'] = top_5_means_df['GDP_means'].round(2)
+    top_5_means_df = top_5_means_df.sort_values(by = "GDP_means", ascending=False)
+
+    print("각 Region별 top5 국가의 평균:")
+    print(top_5_means_df)
+
+def main():
+    path = "/Users/admin/Desktop/Softeer_DE_6th/missions/W1/"
+    
+    with open(path + "etl_project_log.txt", "a") as log_file:
+        log_write(log_file, "ETL Process Started.")
+
+        try:
+            # Extract
+            log_write(log_file, "Start extracting data.")
+            df = extract_data()
+            log_write(log_file, "Finish extracting data.")
+            # 저장
+            df.to_json(path + "Countries_by_GDP.json", orient="records", indent=4, force_ascii = False)
+
+            # Transform
+            log_write(log_file, "Start transforming data.")
+            df = transform_data(df)
+            log_write(log_file, "Finish transforming data.")
+            
+            # Load
+            log_write(log_file, "Start loading data.")
+            load_data(df, path)
+            log_write(log_file, "Finish loading data.")
+            
+        except Exception as e:
+            log_write(log_file, f"Error during extracting data: {e}")
+            raise
+
+        log_write(log_file, "ETL Process Finished.")
+
+    try:
+        region_df = extract_region()
+        region_df = transform_region(region_df)
+        load_region(region_df, path)
+
+        filter_data(path)
+
+    except Exception as e:
+        raise
+
+if __name__ == "__main__":
+    main()
